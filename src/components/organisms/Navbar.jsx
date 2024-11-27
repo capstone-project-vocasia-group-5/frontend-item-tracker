0a1f6d93fd02bdd70abde21235bc8dc017ad01e9
aisyahlailia <aisyahlailia80@gmail.com>
2024-11-27 14:58:33 +0700

    feat:edit foundpage, lostpage, homepage

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -71,7 +71,7 @@ export const Navbar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href=""
                 className="block py-2 px-3 text-primaryGrey rounded-full hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Notifikasi
@@ -79,7 +79,7 @@ export const Navbar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="lost"
                 className="block py-2 px-3 text-primaryGrey rounded-full hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Lost
@@ -87,7 +87,7 @@ export const Navbar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="found"
                 className="block py-2 px-3 text-primaryGrey rounded-full hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Found
