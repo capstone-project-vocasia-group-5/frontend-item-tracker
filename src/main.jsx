40d760ced1363d7453c5b3630a87b13e28a7bdac
Agus Heryanto <agusheryantodev@gmail.com>
2024-12-05 14:36:45 +0700

    feat: integration register user

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import "react-toastify/dist/ReactToastify.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <ToastContainer position="top-right" autoClose={1000} />
+    <ToastContainer position="top-right" autoClose={2000} />
   </StrictMode>
 );
