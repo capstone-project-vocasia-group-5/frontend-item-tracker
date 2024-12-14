b5f223a29393a75cbc0b30f3994ad94045b9f956
adeirawan18 <adeiraw4nn@gmail.com>
2024-12-14 18:30:58 +0700

    Improve: pop up avatar akun

--- src/components/organisms/pop-up-avatar.jsx
+++ src/components/organisms/pop-up-avatar.jsx
@@ -4,30 +4,32 @@ import { Link, useNavigate } from "react-router-dom";
 const PopupAvatar = ({ onLogout }) => {
   const navigate = useNavigate();
 
+  const handleAkun = () => {
+    onLogout(); 
+    navigate("/user"); 
+  };
+
   const handleLogout = () => {
-    onLogout();
-    navigate("/");
+    onLogout(); 
+    navigate("/"); 
   };
 
   return (
     <div className="absolute right-0 mt-2 w-56 bg-white border border-gray-300 rounded-lg shadow-lg z-50">
       <ul className="py-1 px-1">
-        <li>
-          <Link
-            to="/user"
-            className="block px-4  text-start py-3 bg-white text-sm font-extrabold text-gray-800 hover:bg-green-300 hover:text-white transition rounded-lg"
+        <button
+            onClick={handleAkun} 
+            className="w-full bg-white block px-4 py-2 text-sm font-medium text-black hover:bg-blue-500 hover:border-blue-500 hover:text-white transition border-2"
           >
             Akun
-          </Link>
-        </li>
-        <li>
-          <button
-            onClick={handleLogout}
-            className="w-full text-start bg-white block px-4 py-2 text-sm font-medium text-black hover:bg-red-500 hover:border-red-500 hover:text-white transition border-2"
+        </button>
+        <hr className=" w-48 mx-auto border-slate-300" />
+        <button
+            onClick={handleLogout} 
+            className="w-full bg-white block px-4 py-2 text-sm font-medium text-black hover:bg-red-500 hover:border-red-500 hover:text-white transition border-2"
           >
             Keluar
-          </button>
-        </li>
+        </button>
       </ul>
     </div>
   );
